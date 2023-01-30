594aa8657165f5f30a98d6e0baa2f53a1b038257
James Cole <jamestcole@yahoo.co.uk>
2023-01-30 12:28:17 +0000

    main

---
+++ numeric_types.py
@@ -0,0 +1,30 @@
+# Data types
+#Numeric - Ints, Floats, longs, complex
+#String - Text of any type
+#Boolean - True or False
+
+#Operators
+#Arithmetic operators
+# +,-,*,/
+#Comparison operators
+# >,<,==,!=,>=,<=
+
+#Implementing numeric types
+
+#Ints
+
+a=24
+b=16
+
+print(a + b)
+print(a > b)
+print(a < b)
+
+FloatNum = 1.356
+IntNum = 4
+
+print(FloatNum + IntNum)
+
+one_third= 1/3
+print(one_third)
+print(one_third*3)
