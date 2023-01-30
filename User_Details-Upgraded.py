436b3b9484578ac6b853fe0b6198da2b54583976
James Cole <jamestcole@yahoo.co.uk>
2023-01-30 12:39:09 +0000

    main

---
+++ User_Details-Upgraded.py
@@ -0,0 +1,8 @@
+print("What is your name,age,date of birth address and hobbies")
+name = input("name:")
+age = input("age:")
+dob = input("date of birth:")
+address = input("your address is:")
+hobbies = input("your hobbies are:")
+
+print(f"{name}'s age is {age} and their date of birth is {dob}. Their address is {address} and their hobbies are {hobbies}")
